fe045410c01156d74983d62806a890e3379e3c3a
Meaghan Jones and Stephanie Gurung <meaghan@>
2016-07-07 11:49:00 -0700

    passes save spec for vehicle class

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -8,4 +8,18 @@ describe(Vehicle) do
       expect(test_vehicle.make()).to(eq("Mazda"))
     end
   end
+
+  describe('#save') do
+    it('saves a vehicle in an array of vehicles') do
+      test_vehicle = Vehicle.new({:make => "Mazda", :model => "Protege", :year => 2001})
+      expect(test_vehicle.save()).to(eq([test_vehicle]))
+    end
+  end
+
+  describe('.clear') do
+    it("clears the array of vehicles") do
+      test_vehicle = Vehicle.new({:make => "Mazda", :model => "Protege", :year => 2001})
+      expect(test_vehicle.make()).to(eq("Mazda"))
+    end
+  end
 end
