5cf7359d5bd6b708aaf95ad114b9975f1e0338aa
Meaghan Jones and Stephanie Gurung <stephanie@>
2016-07-07 16:27:20 -0700

    fix bugs

--- app.rb
+++ app.rb
@@ -13,27 +13,38 @@ get('/dealerships') do
   erb(:dealerships)
 end
 
-get('/dealerships/:id') do
-  @dealership = Dealership.find(params.fetch('id').to_i())
-  erb(:dealership)
-end
-
 get('/dealerships/new') do
   erb(:dealerships_form)
 end
 
 post('/dealerships') do
   name = params.fetch('name')
-  @dealership = Dealership.new(:name => name).save()
+  @dealership = Dealership.new({:name => name}).save()
   erb(:dealership_success)
 end
 
+get('/dealerships/:id') do
+  @dealership = Dealership.find(params.fetch('id').to_i())
+  erb(:dealership)
+end
+
 get('/dealerships/:id/vehicles/new') do
+  @dealership = Dealership.find(params.fetch('id').to_i())
   erb(:dealership_vehicles_form)
 end
 
-post('/vehicles')
+post('/vehicles') do
   make = params.fetch('make')
   model = params.fetch('model')
-  year = params.fetch('year').to_i()
+  year = params.fetch('year')
   @vehicle = Vehicle.new({:make => make, :model => model, :year => year})
+  @vehicle.save()
+  @dealership = Dealership.find(params.fetch('dealership_id').to_i())
+  @dealership.add_vehicle(@vehicle)
+  erb(:vehicle_success)
+end
+
+get('/vehicles/:id') do
+  @vehicle = Vehicle.find(params.fetch('id').to_i())
+  erb(:vehicle)
+end
