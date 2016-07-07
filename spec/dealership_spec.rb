ff082d697cfb8b4f48c308ac6fe79f5356238b02
Meaghan Jones and Stephanie Gurung <stephanie@>
2016-07-07 11:06:47 -0700

    pass the dealership spec return the car array with the car method

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -12,4 +12,10 @@ describe(Dealership) do
       expect(test_dealership.name()).to(eq("Bob's Used Cars"))
     end
   end
+  describe('#cars') do
+    it('returns the array of cars in the dealership') do
+      test_dealership =  Dealership.new({:name => "Bob's Used Cars", :cars => ["Kia", "Mazda"]})
+      expect(test_dealership.cars()).to(eq(["Kia", "Mazda"]))
+    end
+  end
 end
